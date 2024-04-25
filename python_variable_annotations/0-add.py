510f2a574effa5076176c689535f13806d5a3d79
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-04-25 12:06:37 +0200

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,11 +2,11 @@
 def add(a: float, b: float) -> float:
     """
     Takes two float arguments and returns their sum as a float.
-    
+
     Args:
         a (float): The first float number.
         b (float): The second float number.
-        
+
     Returns:
         float: The sum of a and b.
     """
