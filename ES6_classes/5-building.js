44a5b1b77e069bd270d7f1830a2e6e3f75558d7d
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-07-19 16:03:36 +0200

    vrom

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,25 +1,13 @@
 export default class Building {
     constructor(sqft) {
-      this._sqft = this._validateNumber(sqft, 'Sqft must be a number');
-      if (new.target === Building) {
+      if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
-    }
-  
-    _validateNumber(value, errorMessage) {
-      if (typeof value !== 'number') {
-        throw new TypeError(errorMessage);
-      }
-      return value;
+      this._sqft = sqft;
     }
   
     get sqft() {
       return this._sqft;
-    }
-  
-
-    evacuationWarningMessage() {
-      Error('Cannot instantiate abstract class Building directly');
     }
   }
   
