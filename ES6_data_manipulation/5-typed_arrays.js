098e61115da217c2d356cc1cadc5aab6c43c56b9
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-07-22 12:58:26 +0200

    typed arrays

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,15 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+
+  const dataView = new DataView(buffer);
+
+  dataView.setInt8(position, value);
+
+  return dataView;
+}
+
+export default createInt8TypedArray;
