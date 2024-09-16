6fbac98b8e40b7a7892a84cace924880fcd058ce
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-09-16 10:48:25 +0200

    using process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+// Display welcome message
+console.log("Welcome to Holberton School, what is your name?");
+
+// Listen for user input on stdin
+process.stdin.on('data', (input) => {
+    const name = input.toString().trim();
+    console.log(`Your name is: ${name}`);
+
+    // Close the program with the message
+    console.log("This important software is now closing");
+
+    // Exit the process
+    process.exit();
+});
